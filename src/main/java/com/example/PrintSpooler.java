655f41c9cb645acc143a73e1f667955bd3d2f3f5
Bethan Palmer <bpalmer@toogoodtogo.com>
2023-10-31 11:55:56 +0100

    Add end state for 04_02

--- src/main/java/com/example/PrintSpooler.java
+++ src/main/java/com/example/PrintSpooler.java
@@ -1,6 +1,17 @@
 package com.example;
 
-public class PrintSpooler {
+public final class PrintSpooler {
+
+    private static PrintSpooler INSTANCE;
+
+    private PrintSpooler() {}
+
+    public static PrintSpooler getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new PrintSpooler();
+        }
+        return INSTANCE;
+    }
 
 
     void print() {
