23174551cef011ee6d94306af1e8c8ee9d6bc8d8
Bethan Palmer <bpalmer@toogoodtogo.com>
2023-11-01 16:33:21 +0100

    Add end state for 04_05

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var dog = new Dog();
+        dog.woof();
+
+        var cat = new Cat();
+        cat.meow();
+    }
+
+}
