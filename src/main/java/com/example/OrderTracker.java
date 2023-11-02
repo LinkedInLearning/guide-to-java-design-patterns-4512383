860cd7ecdeed43fc61f716c2eef50bfc9991df58
Bethan Palmer <bpalmer@toogoodtogo.com>
2023-11-02 21:43:04 +0100

    Add begin state for 05_02

--- src/main/java/com/example/OrderTracker.java
+++ src/main/java/com/example/OrderTracker.java
@@ -6,20 +6,20 @@ public class OrderTracker {
 
     public static void main(String[] args) {
 
-        PizzaFlavour hawaiian = new PizzaFlavour("Hawaiian");
+        PizzaFlavor hawaiian = new PizzaFlavor("Hawaiian");
         Pizza hawaiianPizza1 = new Pizza(4, hawaiian);
-        Pizza hawaiianPizza2 = hawaiianPizza1.clone();
-        Pizza hawaiianPizza3 = hawaiianPizza1.clone();
-        Pizza hawaiianPizza4 = hawaiianPizza1.clone();
-        Pizza hawaiianPizza5 = hawaiianPizza1.clone();
+        Pizza hawaiianPizza2 = new Pizza(4, hawaiian);
+        Pizza hawaiianPizza3 = new Pizza(4, hawaiian);
+        Pizza hawaiianPizza4 = new Pizza(4, hawaiian);
+        Pizza hawaiianPizza5 = new Pizza(4, hawaiian);
 
 
-        PizzaFlavour pepperoni = new PizzaFlavour("Pepperoni");
+        PizzaFlavor pepperoni = new PizzaFlavor("Pepperoni");
         Pizza pepperoniPizza1 = new Pizza(5, pepperoni);
-        Pizza pepperoniPizza2 = pepperoniPizza1.clone();
-        Pizza pepperoniPizza3 = pepperoniPizza1.clone();
-        Pizza pepperoniPizza4 = pepperoniPizza1.clone();
-        Pizza pepperoniPizza5 = pepperoniPizza1.clone();
+        Pizza pepperoniPizza2 = new Pizza(5, pepperoni);
+        Pizza pepperoniPizza3 = new Pizza(5, pepperoni);
+        Pizza pepperoniPizza4 = new Pizza(5, pepperoni);
+        Pizza pepperoniPizza5 = new Pizza(5, pepperoni);
 
         List<Meal> order = List.of(hawaiianPizza1, hawaiianPizza2, hawaiianPizza3, hawaiianPizza4, hawaiianPizza5,
                 pepperoniPizza1, pepperoniPizza2, pepperoniPizza3, pepperoniPizza4, pepperoniPizza5);
