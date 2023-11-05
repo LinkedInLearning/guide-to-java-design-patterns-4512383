45e999d3059a7c06df1cb37cfc62c95d45769360
Bethan Palmer <bpalmer@toogoodtogo.com>
2023-11-05 21:44:05 +0100

    Add end state for 06_05

--- src/main/java/com/example/VendingMachine.java
+++ src/main/java/com/example/VendingMachine.java
@@ -1,9 +1,25 @@
 package com.example;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
 public class VendingMachine {
 
+    private static final Map<String, Supplier<Snack>> snackMap = new HashMap<>();
+
+    static {
+        snackMap.put("ChocolateBar", ChocolateBar::new);
+        snackMap.put("Chips", Chips::new);
+        snackMap.put("Drink", Drink::new);
+    }
+
     Snack getSnack(String snackType) {
-        return null;
+        if (snackMap.get(snackType) != null) {
+            return snackMap.get(snackType).get();
+        } else {
+            throw new IllegalArgumentException("Snack type not available");
+        }
     }
 
 
