32b70784ed8c774796df261bc212af420ee8c3a3
Bethan Palmer <bpalmer@toogoodtogo.com>
2023-11-05 19:58:47 +0100

    Add end state for 06_03

--- src/main/java/com/example/TicketMachine.java
+++ src/main/java/com/example/TicketMachine.java
@@ -1,13 +1,25 @@
 package com.example;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
 public class TicketMachine {
 
+
+    private static final Map<String, Supplier<Ticket>> ticketMap = new HashMap<>();
+
+    static {
+        ticketMap.put("Bus", BusTicket::new);
+        ticketMap.put("Train", TrainTicket::new);
+    }
+
     Ticket createTicket(String ticketType) {
-        if(ticketType.equalsIgnoreCase("Bus")) {
-            return new BusTicket();
-        } else {
-            return new TrainTicket();
-        }
+       if (ticketMap.get(ticketType) != null) {
+           return ticketMap.get(ticketType).get();
+       } else {
+           throw new IllegalArgumentException("Invalid ticket type");
+       }
     }
 
 
