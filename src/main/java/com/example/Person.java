86dec6ef18df0bfcc1b2fc38963734ff55bcafa8
Bethan Palmer <bpalmer@toogoodtogo.com>
2023-11-15 21:33:02 +0100

    Add end state for 03_04

--- src/main/java/com/example/Person.java
+++ src/main/java/com/example/Person.java
@@ -12,14 +12,13 @@ public class Person
     private final String emailAddress;
     private final String phoneNumber;
 
-    public Person(String firstName, String middleName, String lastName,
-                  LocalDate dateOfBirth, String emailAddress, String phoneNumber) {
-        this.firstName = firstName;
-        this.middleName = middleName;
-        this.lastName = lastName;
-        this.dateOfBirth = dateOfBirth;
-        this.emailAddress = emailAddress;
-        this.phoneNumber = phoneNumber;
+    private Person(PersonBuilder personBuilder) {
+        this.firstName = personBuilder.firstName;
+        this.middleName = personBuilder.middleName;
+        this.lastName = personBuilder.lastName;
+        this.dateOfBirth = personBuilder.dateOfBirth;
+        this.emailAddress = personBuilder.emailAddress;
+        this.phoneNumber = personBuilder.phoneNumber;
         if (firstName == null || lastName == null) {
             throw new IllegalArgumentException("First name and last name must not be null");
         }
@@ -72,8 +71,7 @@ public class Person
         }
 
         public Person build() {
-            return new Person(firstName, middleName, lastName,
-                    dateOfBirth, emailAddress, phoneNumber);
+            return new Person(this);
         }
 
 
