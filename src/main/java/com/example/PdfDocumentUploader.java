8151884ff8598603e24e09024ecc55cf50aa66f1
Bethan Palmer <bpalmer@toogoodtogo.com>
2023-12-02 13:12:47 +0100

    Add end state for 09_02

--- src/main/java/com/example/PdfDocumentUploader.java
+++ src/main/java/com/example/PdfDocumentUploader.java
@@ -5,6 +5,7 @@ public class PdfDocumentUploader implements DocumentUploader {
 
     @Override
     public void upload() {
+        System.out.println("Preparing PDF document...");
         System.out.println("Uploading PDF document");
     }
 }
