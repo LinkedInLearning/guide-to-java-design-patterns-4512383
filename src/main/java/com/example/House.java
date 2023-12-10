4c3677a5670eb475696d174855feaa66bfd17add
Bethan Palmer <bpalmer@toogoodtogo.com>
2023-12-10 16:27:48 +0100

    Add 12_04

---
+++ src/main/java/com/example/House.java
@@ -0,0 +1,20 @@
+package com.example;
+
+public class House {
+
+    public static void main(String[] args) {
+
+        var room = new BasicRoom();
+        room.printFurniture();
+
+        System.out.println();
+
+        // create a room with carpet, curtains and a couch
+
+        System.out.println();
+
+        // create a room with carpet, curtains, a couch and a table
+
+    }
+
+}
