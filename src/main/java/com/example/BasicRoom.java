9c6f993691de525c33be0b96074cf2c9a5686a44
Bethan Palmer <bpalmer@toogoodtogo.com>
2023-12-10 16:27:48 +0100

    Add 12_04

---
+++ src/main/java/com/example/BasicRoom.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class BasicRoom implements Room {
+
+    @Override
+    public void printFurniture() {
+        System.out.println("Carpet");
+    }
+
+}
---
+++ src/main/java/com/example/House.java
@@ -0,0 +1,20 @@
+package com.example;
+
+public class House {
+
+    public static void main(String[] args) {
+
+        var room = new BasicRoom();
+        room.printFurniture();
+
+        System.out.println();
+
+        // create a room with a carpet and a couch
+
+        System.out.println();
+
+        // create a room with a carpet, a couch and a table
+
+    }
+
+}
