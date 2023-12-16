51866e3d29dbfec7057403d9533ea692c00746d2
Bethan Palmer <bpalmer@toogoodtogo.com>
2023-12-16 19:30:35 +0100

    Add end state for 13_04

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,19 +4,8 @@ public class App {
 
     public static void main(String[] args) {
 
-        var client = new Client();
-        client.connectToServer();
-
-        var clientServer = new ClientServer();
-        clientServer.authenticateClient();
-        clientServer.routeToRecipient();
-
-        var recipientServer = new RecipientServer();
-        recipientServer.authenticateRecipient();
-        recipientServer.sendMessage();
-
-        var recipient = new Recipient();
-        recipient.receiveMessage();
+        var sendMessageFacade = new SendMessageFacade();
+        sendMessageFacade.sendMessage();
 
     }
 
