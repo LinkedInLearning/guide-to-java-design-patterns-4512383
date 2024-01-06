02c1771f04078d0da3d9f0cfcd4c264af2dc5d78
Bethan Palmer <bpalmer@toogoodtogo.com>
2024-01-06 12:50:36 +0100

    WIP

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,19 +3,15 @@ package com.example;
 public class App {
 
     public static void main(String[] args) {
-
         var item1 = new Item(1);
         var item2 = new Item(2);
         var item3 = new Item(3);
 
-        var orderOperationExecutor = new ItemOperationExecutor();
-        orderOperationExecutor.queueOperation(new AddItemToBasketOperation(item1));
-        orderOperationExecutor.queueOperation(new AddItemToBasketOperation(item2));
-        orderOperationExecutor.queueOperation(new AddItemToBasketOperation(item3));
-
-        orderOperationExecutor.queueOperation(new RemoveItemFromBasketOperation(item2));
-
-        orderOperationExecutor.checkout();
+        var itemOperationExecutor = new ItemOperationExecutor();
+        itemOperationExecutor.doOperation(new AddItemToBasketOperation(item1));
+        itemOperationExecutor.doOperation(new AddItemToBasketOperation(item2));
+        itemOperationExecutor.doOperation(new AddItemToBasketOperation(item3));
+        itemOperationExecutor.doOperation(new RemoveItemFromBasketOperation(item2));
     }
 
 }
