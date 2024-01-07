c0dcabe680f7e3812ffd7067a3f281a9b693d05e
Bethan Palmer <bpalmer@toogoodtogo.com>
2024-01-07 11:51:58 +0100

    Add end state for 20_02

---
+++ src/main/java/com/example/Inventory.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import java.util.Iterator;
+
+public class Inventory implements Iterable<Item> {
+
+    private final Item[] items;
+
+    public Inventory(Item... items) {
+        this.items = items;
+    }
+
+    public Item[] getItems() {
+        return this.items;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return null;
+    }
+
+}
