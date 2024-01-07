6a3b6cb7c91c3fdeef2636ed25b1ea322f36f8f4
Bethan Palmer <bpalmer@toogoodtogo.com>
2024-01-07 13:20:33 +0100

    Add 20_04

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,11 +4,11 @@ public class App {
 
     public static void main(String[] args) {
 
-        var pen = new Item("Pen", 20);
-        var pencil = new Item("Pencil", 0);
-        var eraser = new Item("Eraser", 15);
-
-
+        var india = new Country("India");
+        var poland = new Country("Poland");
+        var nigeria = new Country("Nigeria");
+        var canada = new Country("Canada");
+        var argentina = new Country("Argentina");
 
     }
 
---
+++ src/main/java/com/example/Country.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class Country {
+
+    private final String name;
+
+    public Country(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+}
