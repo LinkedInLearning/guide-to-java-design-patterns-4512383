781804ebcee5452b1ad024ad61ecc6973bbea5b2
Bethan Palmer <bpalmer@toogoodtogo.com>
2024-01-13 11:13:14 +0100

    Add begin state for 21_02

--- src/main/java/com/example/DocumentViewer.java
+++ src/main/java/com/example/DocumentViewer.java
@@ -5,11 +5,8 @@ public class DocumentViewer {
     public static void main(String[] args) {
         var textBlock = new TextDocument();
         textBlock.write("Hello, world! ");
-        textBlock.save();
         textBlock.print();
-        textBlock.write("How are you? ");
-        textBlock.print();
-        textBlock.undo();
+        textBlock.write("How are you?");
         textBlock.print();
     }
 
--- src/main/java/com/example/TextDocument.java
+++ src/main/java/com/example/TextDocument.java
@@ -3,7 +3,6 @@ package com.example;
 public class TextDocument {
 
     private String text = "";
-    private TextDocumentState state = new TextDocumentState(text);
 
     public void write(String text) {
         this.text += text;
@@ -11,14 +10,6 @@ public class TextDocument {
 
     public void print() {
         System.out.println(text);
-    }
-
-    public void save() {
-        state.setState(text);
-    }
-
-    public void undo() {
-        text = state.getState();
     }
 
 }
