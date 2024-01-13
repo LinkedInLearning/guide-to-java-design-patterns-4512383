6a33a1a55f9912b96e8f0e3221cb4ccdd79f07ec
Bethan Palmer <bpalmer@toogoodtogo.com>
2024-01-13 11:57:52 +0100

    Add 22_03

---
+++ src/main/java/com/example/Item.java
@@ -0,0 +1,19 @@
+package com.example;
+
+public class Item {
+
+    private int price;
+
+    public Item(int price) {
+        this.price = price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+}
---
+++ src/main/java/com/example/Store.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class Store {
+
+    public static void main(String[] args) {
+        var item = new Item(20);
+//        item.save();
+        System.out.println("Item price is " + item.getPrice());
+        item.setPrice(10);
+        System.out.println("Item price is " + item.getPrice());
+//        item.undo();
+//        System.out.println("Item price is " + item.getPrice());
+    }
+
+}
