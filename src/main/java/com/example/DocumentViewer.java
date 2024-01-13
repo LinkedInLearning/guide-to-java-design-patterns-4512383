f9c19e2e2479933a209fc7310087ff9c14211fb0
Bethan Palmer <bpalmer@toogoodtogo.com>
2024-01-13 11:13:14 +0100

    Add begin state for 22_02

--- src/main/java/com/example/DocumentViewer.java
+++ src/main/java/com/example/DocumentViewer.java
@@ -3,14 +3,11 @@ package com.example;
 public class DocumentViewer {
 
     public static void main(String[] args) {
-        var textBlock = new TextDocument();
-        textBlock.write("Hello, world! ");
-        textBlock.save();
-        textBlock.print();
-        textBlock.write("How are you? ");
-        textBlock.print();
-        textBlock.undo();
-        textBlock.print();
+        var textDocument = new TextDocument();
+        textDocument.write("Hello, world! ");
+        textDocument.print();
+        textDocument.write("How are you?");
+        textDocument.print();
     }
 
 }
