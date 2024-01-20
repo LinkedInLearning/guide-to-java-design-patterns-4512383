0d4f8f4c43d2d10f3df0cec06fe5492f09e976d3
Bethan Palmer <bpalmer@toogoodtogo.com>
2024-01-20 10:09:49 +0100

    Add begin state for 24_02

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,12 +1,21 @@
 package com.example;
 
+import java.util.List;
+
 public class App {
 
     public static void main(String[] args) {
-        var restaurant = new Restaurant();
-        restaurant.serveCustomer();
-        restaurant.openRestaurant();
-        restaurant.serveCustomer();
+
+        var auction = new Auction();
+        auction.startAuction();
+        auction.placeBid();
+
+        auction.startAuction();
+
+        auction.closeAuction();
+        auction.placeBid();
+
+        auction.closeAuction();
     }
 
 }
