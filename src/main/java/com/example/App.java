bca7101f73671dbcb4b02d064d372de87f482c33
Bethan Palmer <bpalmer@toogoodtogo.com>
2024-01-20 10:09:49 +0100

    Add 24_02

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,10 +3,18 @@ package com.example;
 public class App {
 
     public static void main(String[] args) {
-        var restaurant = new Restaurant();
-        restaurant.serveCustomer();
-        restaurant.openRestaurant();
-        restaurant.serveCustomer();
+
+        var auction = new Auction();
+        auction.startAuction();
+        auction.placeBid();
+
+        auction.startAuction();
+
+        auction.closeAuction();
+        auction.placeBid();
+
+        auction.closeAuction();
+
     }
 
 }
--- src/main/java/com/example/RestaurantState.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-public interface RestaurantState {
-
-    void serveCustomer(Restaurant restaurant);
-
-}
