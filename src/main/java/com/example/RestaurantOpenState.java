8d849ec941bd5c982b2c52e70c887c903e586b4b
Bethan Palmer <bpalmer@toogoodtogo.com>
2024-01-20 10:09:49 +0100

    Add 24_02

--- src/main/java/com/example/RestaurantOpenState.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-public class RestaurantOpenState implements RestaurantState {
-    @Override
-    public void serveCustomer(Restaurant restaurant) {
-        System.out.println("Serving customer");
-    }
-}
