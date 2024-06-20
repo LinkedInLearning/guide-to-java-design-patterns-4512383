563163341af29333df34672716d6288a35f2fd2f
Sonja Lechner <blaquekaktus@gmail.com>
2024-06-20 13:03:44 +0000

    Initial Commit

--- src/main/java/com/example/PrintSpooler.java
+++ src/main/java/com/example/PrintSpooler.java
@@ -2,6 +2,8 @@ package com.example;
 
 public class PrintSpooler {
 
+    private PrintSpooler(){}
+    private static PrintSpooler INSTANCE;
 
     void print() {
         System.out.println("Printing...");
